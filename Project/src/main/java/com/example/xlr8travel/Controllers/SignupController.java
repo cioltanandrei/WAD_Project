1674682a155c77a8333b6e262277056a680ed0a2
Razvan <razvan.uleia@gmail.com>
2024-04-16 11:23:11 +0300

    added login, signup and page navigation

--- Project/src/main/java/com/example/xlr8travel/Controllers/SignupController.java
+++ Project/src/main/java/com/example/xlr8travel/Controllers/SignupController.java
@@ -1,15 +1,16 @@
 package com.example.xlr8travel.Controllers;
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/index")
-public class HomeController {
+@RequestMapping(value = "/signup")
+public class SignupController {
     @GetMapping
-    public String list(final Model model) {
-        return "index.html";
+    public ModelAndView getTestData() {
+        ModelAndView mv = new ModelAndView();
+        return mv;
     }
 }
